aa97108b89c453564ada6e784ccd7713bda60440
hitode909 <hitode909@gmail.com>
2011-07-12 13:54:00 +0900

    塗り方落ち着いた感じに，0は赤

--- webserver/public/js/content.coffee
+++ webserver/public/js/content.coffee
@@ -2,10 +2,15 @@ fill_pattern = 'color'
 last_res = null
 
 get_color = (level) ->
+  level = 0.0 if level < 0.0
+  level = 1.0 if level > 1.0
   if fill_pattern == 'color'
-    level = 1.0 - level
-    h =  300 - level * 300.0
-    l = if level < 0.1 then level / 0.1 * 50 else 50
+    rlevel = 1.0 - level
+    h =  if level > 0.0 then 90 + rlevel * 180.0 else 0.0
+    # factor = 0.9
+    # l = if level < 0.1 then 50 else 0
+    # l = if rlevel < factor then rlevel / factor * 50 else 50
+    l = rlevel * 50
     "hsl(#{ h }, 100%, #{ l }%)"
   else
     "hsl(0, 0%, #{ level * 90 }%)"
@@ -14,19 +19,21 @@ highlight = (res) ->
   fragment = document.createDocumentFragment()
   total = res.total
 
-  max = 0
-  $.each res.tokens, (i, data) ->
-    {value, count, rate} = data
-    max = count if max < count
+  max = 0.01
+  # $.each res.tokens, (i, data) ->
+  #   {value, count, rate} = data
+  #   max = count if max < count
 
   $.each res.tokens, (i, data) ->
     {value, count, rate} = data
 
-    level = Math.log(count) / Math.log(max)
+    level = rate * 10
+    level = Math.log(rate+1) / Math.log(max+1)
+    level = rate / max
     level = 0 if isNaN(level) or level == Infinity or level == -Infinity
     color = get_color(level)
     title = if value.match(/\S/) then count else ''
-    node = $('<span>').addClass('token').text(value).attr('title', title).css
+    node = $('<span>').addClass('token').text(value).attr('title', "#{ title } (#{ rate * 100 })").css
       color: color
     fragment.appendChild(node[0])
   $('#result').empty().append(fragment)
--- webserver/public/js/content.js
+++ webserver/public/js/content.js
@@ -2,11 +2,17 @@ var fill_pattern, get_color, highlight, last_res, preview_color;
 fill_pattern = 'color';
 last_res = null;
 get_color = function(level) {
-  var h, l;
+  var h, l, rlevel;
+  if (level < 0.0) {
+    level = 0.0;
+  }
+  if (level > 1.0) {
+    level = 1.0;
+  }
   if (fill_pattern === 'color') {
-    level = 1.0 - level;
-    h = 300 - level * 300.0;
-    l = level < 0.1 ? level / 0.1 * 50 : 50;
+    rlevel = 1.0 - level;
+    h = level > 0.0 ? 90 + rlevel * 180.0 : 0.0;
+    l = rlevel * 50;
     return "hsl(" + h + ", 100%, " + l + "%)";
   } else {
     return "hsl(0, 0%, " + (level * 90) + "%)";
@@ -16,24 +22,19 @@ highlight = function(res) {
   var fragment, max, total;
   fragment = document.createDocumentFragment();
   total = res.total;
-  max = 0;
-  $.each(res.tokens, function(i, data) {
-    var count, rate, value;
-    value = data.value, count = data.count, rate = data.rate;
-    if (max < count) {
-      return max = count;
-    }
-  });
+  max = 0.01;
   $.each(res.tokens, function(i, data) {
     var color, count, level, node, rate, title, value;
     value = data.value, count = data.count, rate = data.rate;
-    level = Math.log(count) / Math.log(max);
+    level = rate * 10;
+    level = Math.log(rate + 1) / Math.log(max + 1);
+    level = rate / max;
     if (isNaN(level) || level === Infinity || level === -Infinity) {
       level = 0;
     }
     color = get_color(level);
     title = value.match(/\S/) ? count : '';
-    node = $('<span>').addClass('token').text(value).attr('title', title).css({
+    node = $('<span>').addClass('token').text(value).attr('title', "" + title + " (" + (rate * 100) + ")").css({
       color: color
     });
     return fragment.appendChild(node[0]);
