f7d4bb01c6a354128e73d0cc79d1c0ab83404787
hitode909 <hitode909@gmail.com>
2011-09-26 15:43:16 +0900

    most_watchedでソート

--- lib/shikoku/github_crawler.rb
+++ lib/shikoku/github_crawler.rb
@@ -7,7 +7,7 @@ module Shikoku
 
     def self.each(lang = 'Ruby')
       (0..(1/0.0)).each{ |page|
-        doc = Nokogiri open("https://github.com/languages/#{lang}/created?page=#{page}")
+        doc = Nokogiri open("https://github.com/languages/#{lang}/most_watched?page=#{page}")
         doc.search('.title').each{ |line|
           path = line.search('a')[0]['href']
           yield "git://github.com#{path}.git"
