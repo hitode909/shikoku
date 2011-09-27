4f7038b833cd5e3eb1ce4ed8f72ec05ddf8d0356
hitode909 <hitode909@gmail.com>
2011-09-27 17:54:26 +0900

    rails crawler

--- lib/shikoku/github_crawler.rb
+++ lib/shikoku/github_crawler.rb
@@ -15,4 +15,22 @@ module Shikoku
       }
     end
   end
+
+  class RailsCrawler
+
+    def self.each(lang = 'Ruby')
+      (1..(1/0.0)).each{ |page|
+        doc = Nokogiri open("https://github.com/search?langOverride=&language=Ruby&q=rails&repo=&start_value=#{page}&type=Repositories&x=0&y=0")
+        p doc.at('.title')
+        doc.search('.title').each{ |line|
+          begin
+            path = line.search('a')[0]['href']
+            yield "git://github.com#{path}.git"
+          rescue => error
+            warn 'skip'
+          end
+        }
+      }
+    end
+  end
 end
