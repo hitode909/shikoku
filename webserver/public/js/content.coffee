6ff00026c4810eb99036a31bc2a01c36d2d5b31e
hitode909 <hitode909@gmail.com>
2011-12-17 16:17:08 +0900

    up

--- webserver/public/js/content.coffee
+++ webserver/public/js/content.coffee
@@ -1,78 +1,14 @@
-fill_pattern = 'color'
-fill_factor = 180.0
-color_sample_length = 500
-
-throttle = (fn, delay) ->
-  timer = null
-  ->
-    return if timer
-    context = this
-    args = arguments
-    timer = setTimeout ->
-      timer = null
-      fn.apply context, args
-    ,delay
-
-
-get_color = (level) ->
-  level = 0.0 if level < 0.0
-  level = 1.0 if level > 1.0
-  # level = Math.sin(level * Math.PI / 2)
-  # level = Math.log(level + 1.0) / Math.log(2.0)
-  if fill_pattern == 'color'
-    rlevel = 1.0 - level
-    # fill_factor * 0.5 のところ いい感じにしたい
-    h =  if level > 0.0 then fill_factor * 0.5 + rlevel * fill_factor else 0.0
-    l = (1.0 - Math.pow(level, 5)) * 50
-    "hsl(#{ h }, 100%, #{ l }%)"
-  else
-    "hsl(0, 0%, #{ level * 90 }%)"
-
-cache = {}
-
-get_random_color = ->
-  console.log('get_random')
-  h = Math.random() * 360
-  "hsl(#{ h }, 50%, 50%)"
-
 get_color_from_token_class = (token_class) ->
   sum = 0
   for i in [0..token_class.length-1]
     sum += token_class.charCodeAt(i) * i
   h = sum % 31 * 11
-  console.log(token_class, h, sum)
 
   "hsl(#{ h }, 50%, 50%)"
 
 create_token = (def) ->
   $('<span>').addClass('token').text(def.value).attr(title: def.token_class).css
       color: get_color_from_token_class(def.token_class)
-
-highlight_histogram = (res) ->
-  fragment = document.createDocumentFragment()
-  summary = {}
-  $.each res.tokens, (i, data) ->
-    return unless data.value.match(/\S/)
-    index = Math.floor(data.rate * color_sample_length)
-    summary[index] ?= 0
-    summary[index] += 1 / res.tokens.length
-
-  list = []
-  for i in [0..color_sample_length]
-    list[i] = summary[i] || 0
-
-  preview_color_by_summary(round_list(list, 200))
-
-round_list = (list, range) ->
-  res = []
-  for i in [0..(list.length-1)]
-    v = 0
-    for j in [Math.floor(i - range*0.5)..Math.floor(i + range*0.5)]
-      if 0 <= j && j < list.length && list[j] > 0
-        v += list[j] * Math.pow(0.9,  Math.abs(i - j))
-    res[i] = v
-
-  res
 
 highlight = (res) ->
   if res.is_valid
@@ -92,28 +28,12 @@ highlight = (res) ->
   return
   highlight_histogram(res)
 
-preview_color = ->
-  $('#color-sample').empty()
-  for i in [0..color_sample_length]
-    $('#color-sample').append $('<span>').attr('data-rate-index', i).css
-      background: get_color(i / color_sample_length)
-
-preview_color_by_summary = (summary)->
-  $('#color-sample').empty()
-  for i in [0..color_sample_length]
-    height = 200 * (Math.log(summary[i] + 1) / Math.log(2.0))
-    $('#color-sample').append $('<span>').attr('data-rate-index', i).css
-      height: "#{height}px"
-      background: get_color(i / color_sample_length + 0.0001)
-
 $ ->
   last = ''
   last_res = null
 
-  fill_factor = $('#fill-factor').val()
-
   setInterval ->
-    body = $('form').find('textarea').val()
+    body = $('#source-code').val()
     if last == body
       return
     last = body
@@ -121,41 +41,7 @@ $ ->
       body: body
       mime_type: 'application/ruby'
       (res) ->
+        return if body != $('#source-code').val()
         last_res = res
         highlight(res)
   , 1000
-
-  return
-
-  $('input[name="fill-type"]').change ->
-    fill_pattern = $(this).val()
-    preview_color()
-    highlight(last_res)
-
-  preview_color()
-
-  selected_token = null
-
-  completions_container = $('#completions-container')
-
-  $('.token').live 'click', (event) ->
-
-    selected_token = $(this).text()
-
-    $('#result').css
-      background: '#ddd'
-    $.post '/focus'
-      body: $('form').find('textarea').val()
-      focus: selected_token
-      mime_type: 'application/ruby'
-      (res) ->
-        $('#result').css
-          background: ''
-        last_res = res
-        highlight(res)
-
-  $('#fill-factor').change throttle((event) ->
-    fill_factor = $(this).val()
-    preview_color()
-    highlight(last_res) if last_res
-  , 100)
--- webserver/public/js/content.js
+++ webserver/public/js/content.js
@@ -1,47 +1,4 @@
-var cache, color_sample_length, create_token, fill_factor, fill_pattern, get_color, get_color_from_token_class, get_random_color, highlight, highlight_histogram, preview_color, preview_color_by_summary, round_list, throttle;
-fill_pattern = 'color';
-fill_factor = 180.0;
-color_sample_length = 500;
-throttle = function(fn, delay) {
-  var timer;
-  timer = null;
-  return function() {
-    var args, context;
-    if (timer) {
-      return;
-    }
-    context = this;
-    args = arguments;
-    return timer = setTimeout(function() {
-      timer = null;
-      return fn.apply(context, args);
-    }, delay);
-  };
-};
-get_color = function(level) {
-  var h, l, rlevel;
-  if (level < 0.0) {
-    level = 0.0;
-  }
-  if (level > 1.0) {
-    level = 1.0;
-  }
-  if (fill_pattern === 'color') {
-    rlevel = 1.0 - level;
-    h = level > 0.0 ? fill_factor * 0.5 + rlevel * fill_factor : 0.0;
-    l = (1.0 - Math.pow(level, 5)) * 50;
-    return "hsl(" + h + ", 100%, " + l + "%)";
-  } else {
-    return "hsl(0, 0%, " + (level * 90) + "%)";
-  }
-};
-cache = {};
-get_random_color = function() {
-  var h;
-  console.log('get_random');
-  h = Math.random() * 360;
-  return "hsl(" + h + ", 50%, 50%)";
-};
+var create_token, get_color_from_token_class, highlight;
 get_color_from_token_class = function(token_class) {
   var h, i, sum, _ref;
   sum = 0;
@@ -49,7 +6,6 @@ get_color_from_token_class = function(token_class) {
     sum += token_class.charCodeAt(i) * i;
   }
   h = sum % 31 * 11;
-  console.log(token_class, h, sum);
   return "hsl(" + h + ", 50%, 50%)";
 };
 create_token = function(def) {
@@ -58,41 +14,6 @@ create_token = function(def) {
   }).css({
     color: get_color_from_token_class(def.token_class)
   });
-};
-highlight_histogram = function(res) {
-  var fragment, i, list, summary;
-  fragment = document.createDocumentFragment();
-  summary = {};
-  $.each(res.tokens, function(i, data) {
-    var index, _ref;
-    if (!data.value.match(/\S/)) {
-      return;
-    }
-    index = Math.floor(data.rate * color_sample_length);
-    if ((_ref = summary[index]) == null) {
-      summary[index] = 0;
-    }
-    return summary[index] += 1 / res.tokens.length;
-  });
-  list = [];
-  for (i = 0; 0 <= color_sample_length ? i <= color_sample_length : i >= color_sample_length; 0 <= color_sample_length ? i++ : i--) {
-    list[i] = summary[i] || 0;
-  }
-  return preview_color_by_summary(round_list(list, 200));
-};
-round_list = function(list, range) {
-  var i, j, res, v, _ref, _ref2, _ref3;
-  res = [];
-  for (i = 0, _ref = list.length - 1; 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
-    v = 0;
-    for (j = _ref2 = Math.floor(i - range * 0.5), _ref3 = Math.floor(i + range * 0.5); _ref2 <= _ref3 ? j <= _ref3 : j >= _ref3; _ref2 <= _ref3 ? j++ : j--) {
-      if (0 <= j && j < list.length && list[j] > 0) {
-        v += list[j] * Math.pow(0.9, Math.abs(i - j));
-      }
-    }
-    res[i] = v;
-  }
-  return res;
 };
 highlight = function(res) {
   var fragment;
@@ -115,38 +36,13 @@ highlight = function(res) {
   return;
   return highlight_histogram(res);
 };
-preview_color = function() {
-  var i, _results;
-  $('#color-sample').empty();
-  _results = [];
-  for (i = 0; 0 <= color_sample_length ? i <= color_sample_length : i >= color_sample_length; 0 <= color_sample_length ? i++ : i--) {
-    _results.push($('#color-sample').append($('<span>').attr('data-rate-index', i).css({
-      background: get_color(i / color_sample_length)
-    })));
-  }
-  return _results;
-};
-preview_color_by_summary = function(summary) {
-  var height, i, _results;
-  $('#color-sample').empty();
-  _results = [];
-  for (i = 0; 0 <= color_sample_length ? i <= color_sample_length : i >= color_sample_length; 0 <= color_sample_length ? i++ : i--) {
-    height = 200 * (Math.log(summary[i] + 1) / Math.log(2.0));
-    _results.push($('#color-sample').append($('<span>').attr('data-rate-index', i).css({
-      height: "" + height + "px",
-      background: get_color(i / color_sample_length + 0.0001)
-    })));
-  }
-  return _results;
-};
 $(function() {
-  var completions_container, last, last_res, selected_token;
+  var last, last_res;
   last = '';
   last_res = null;
-  fill_factor = $('#fill-factor').val();
-  setInterval(function() {
+  return setInterval(function() {
     var body;
-    body = $('form').find('textarea').val();
+    body = $('#source-code').val();
     if (last === body) {
       return;
     }
@@ -155,41 +51,11 @@ $(function() {
       body: body,
       mime_type: 'application/ruby'
     }, function(res) {
+      if (body !== $('#source-code').val()) {
+        return;
+      }
       last_res = res;
       return highlight(res);
     });
   }, 1000);
-  return;
-  $('input[name="fill-type"]').change(function() {
-    fill_pattern = $(this).val();
-    preview_color();
-    return highlight(last_res);
-  });
-  preview_color();
-  selected_token = null;
-  completions_container = $('#completions-container');
-  $('.token').live('click', function(event) {
-    selected_token = $(this).text();
-    $('#result').css({
-      background: '#ddd'
-    });
-    return $.post('/focus', {
-      body: $('form').find('textarea').val(),
-      focus: selected_token,
-      mime_type: 'application/ruby'
-    }, function(res) {
-      $('#result').css({
-        background: ''
-      });
-      last_res = res;
-      return highlight(res);
-    });
-  });
-  return $('#fill-factor').change(throttle(function(event) {
-    fill_factor = $(this).val();
-    preview_color();
-    if (last_res) {
-      return highlight(last_res);
-    }
-  }, 100));
 });
