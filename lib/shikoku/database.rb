3d61ea15f342d74b63657420f59b16338a96026d
hitode909 <hitode909@gmail.com>
2011-12-17 18:56:36 +0900

    up

--- lib/shikoku/database.rb
+++ lib/shikoku/database.rb
@@ -5,16 +5,15 @@ require 'logger'
 module Shikoku
   class Database
     class << self
-      def database
-        # logger = Logger.new($stdout)
-        # logger.level = Logger::DEBUG
-        # , :logger => logger
-        @collection ||= Mongo::Connection.new('localhost', 27017).db("shikoku")
-      end
-
       def collection(mime_type)
         database.collection(mime_type)
       end
+
+      private
+      def database
+        @collection ||= Mongo::Connection.new('localhost', 27017).db("shikoku")
+      end
+
     end
   end
 end
