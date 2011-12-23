e6f59a8220526ade0e357e7de37031e9c93d22b1
hitode909 <hitode909@gmail.com>
2011-12-23 21:54:11 +0900

    これもいる Revert "delete"
    This reverts commit 403037a414a99a940b8a8124f6f87e5d04f083d7.

---
+++ script/update_local.rb
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+self_file =
+  if File.symlink?(__FILE__)
+    require 'pathname'
+    Pathname.new(__FILE__).realpath
+  else
+    __FILE__
+  end
+$:.unshift(File.dirname(self_file) + "/../lib")
+
+require 'bundler/setup'
+require 'shikoku'
+
+Shikoku::Repository.all.each{ |repos|
+  puts repos.remote_url
+  begin
+    repos.setup
+  rescue => error
+    warn error
+  end
+}
